3362ecc0723a6100cad19cb0b2427ac63df7e49a
JesperJager1986 <jesperjag86@gmail.com>
2022-04-26 16:58:55 +0200

    create package script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='TF_package_management',
+    version='0.0.1',
+    auther='Jesper Thoft Illemann Jæger',
+    author_email='jesperjag86@gmail.com',
+    description='package manegement system on github',
+    url='https://github.com/JesperJager1986/testing_package_management',
+    package_dir={"": "src"}
+)
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def testing():
+    print('from testing')
+
+
+if '__main__' == __name__:
+    print('hallo from main')
